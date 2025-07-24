b3b958457ae38237bad86d762cd0313545f3ce00
huhuangcheng <126866776+huhuangcheng@users.noreply.github.com>
2025-07-25 00:02:37 +0800

    Update vue.config.js

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -2,15 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  //反向代理
-  devServer:{
-    proxy:{
-      "/adminapi":{
-        target:"http://localhost:3000",
-        changeOrigin:true
-      }
-    }
-  }
+
 })
 
 
